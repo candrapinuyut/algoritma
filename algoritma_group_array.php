1415dfce7f2ee2f76e2177e97ecfb3e03bbf2db2
candrapinuyut <candrapinuyut@gmail.com>
2023-01-24 10:59:01 +0800

    Update algoritma_group_array.php

--- algoritma_group_array.php
+++ algoritma_group_array.php
@@ -1,18 +1,17 @@
 <?php 
 
-$items=[1,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4];
+  $items=[1,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4];
 
-$temp='';
-$y='';
+  $temp='';
+  $y='';
 
-foreeach($items as $i){
-  if($y==$i) 
-    $temp='';
-  else 
-    $temp=$i;
-   $y=$i;
-   
-   echo $i.'<br />';
-   
-}
- 
+  foreach($items as $i){
+    if($y==$i) 
+        $temp='';
+    else 
+        $temp=$i;
+    $y=$i;
+
+    echo $temp.'<br />';
+
+  }
