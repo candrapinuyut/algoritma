03909b728386f7e154bd6061e218a1219de472ff
candrapinuyut <candrapinuyut@gmail.com>
2023-01-24 11:03:01 +0800

    Update algoritma_group_array.php

--- algoritma_group_array.php
+++ algoritma_group_array.php
@@ -1,5 +1,32 @@
 <?php 
+ /* 
+  cocok untuk melakukan group pada data dari database.
+  contoh data :
+  ----------------------------------------
+  no.  kec                       kelurahan
+  ---------------------------------------
+  1.   kotamobagu barat          molinow
+  2.   kotamobagu barat          mongkonai
+  3.   kotamobagu barat          mogolaing
+  4.   kotamobagu timur          matali
+  5.   kotamobagu timur          Sinindian
+  6.   kotamobagu timur          Tumubui
 
+
+  
+  dengan grouping data di atas, sebenarny kita bisa sederhanakan dengan:
+   ----------------------------------------
+  no.  kec                       kelurahan
+  ---------------------------------------
+  1.   kotamobagu barat          molinow
+                                 mongkonai
+                                 mogolaing
+  2.   Kotamobagu Timur          Matali
+                                 Sinindian
+                                 Tumubui
+ /* 
+ 
+ 
   $items=[1,1,1,1,1,2,2,2,2,2,3,3,3,3,4,4,4,4];
 
   $temp='';
